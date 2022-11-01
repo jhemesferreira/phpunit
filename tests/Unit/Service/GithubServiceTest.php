d89e73b4bd5c96a989917d3b1ff2b90a92491901
jhemesferreira <fs.jhemes@gmail.com>
2022-11-01 10:11:14 +0100

    :hammer: dev: testing githubService with mock/stub

---
+++ tests/Unit/Service/GithubServiceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tests\Unit\Service;
+
+use App\Enum\HealthStatus;
+use App\Service\GithubService;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class GithubServiceTest extends TestCase
+{
+    /**
+     * @dataProvider dinoNameProvider
+     */
+    public function testGetHealthReportReturnsCorrectHealthStatusForDino(HealthStatus $expectedStatus, string $dinoName): void
+    {
+        $mockLogger = $this->createMock(LoggerInterface::class);
+        $mockHttpClient = $this->createMock(HttpClientInterface::class);
+        $mockResponse = $this->createMock(ResponseInterface::class);
+
+        $mockResponse
+            ->method('toArray')
+            ->willReturn([
+                    [
+                        'title' => 'Daisy',
+                        'labels' => [['name' => 'Status: Sick']],
+                    ],
+                    [
+                        'title' => 'Maverick',
+                        'labels' => [['name' => 'Status: Healthy']],
+                    ],
+                ]);
+
+
+        $mockHttpClient
+            ->method('request')
+            ->willReturn($mockResponse);
+
+
+        $service = new GithubService(logger: $mockLogger, httpClient: $mockHttpClient);
+
+        self::assertSame($expectedStatus, $service->getHealthReport($dinoName));
+    }
+
+
+    public function dinoNameProvider(): \Generator
+    {
+        yield 'Sick Dino' => [
+            HealthStatus::SICK,
+            'Daisy',
+        ];
+
+        yield 'Healthy Dino' => [
+            HealthStatus::HEALTHY,
+            'Maverick',
+        ];
+    }
+}
